113e3e35dd50720d534460a5757658c4bbe075c5
panuwatmangsang <48234038+panuwatmangsang@users.noreply.github.com>
2022-01-21 14:43:41 +0700

    add marker

--- resources/views/car_gps.blade.php
+++ resources/views/car_gps.blade.php
@@ -14,32 +14,61 @@
   <script>
     function myMap() {
       var position = {
-        lat: 18.8945854,
-        lng: 99.0098195
+        lat: 18.9306783,
+        lng: 99.1841769
       }
+      var maps;
+
+      var locations = [
+        ['Toyota', 18.8945854, 99.0098195],
+        ['Isuzu', 18.8935056, 98.9871569],
+        ['Honda', 18.8902647, 98.9852353]
+      ];
 
       var mapProp = {
-        // center: new google.maps.LatLng(18.8945854, 99.0098195),
-        center: position,
+        center: new google.maps.LatLng(18.9306783, 99.1841769),
+        // center: position,
         zoom: 10,
       };
 
       // แสดงแผนที่
-      var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+      var maps = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
       // แสดง/สรา้ง marker
       var marker = new google.maps.Marker({
         position: position,
-        map:map
+        map: maps
       });
 
       // แสดง info
       var info = new google.maps.InfoWindow({
         content: '<div style="font-size:15px;">Toyota</div>'
       });
-      google.maps.event.addListener(marker,'click',function() {
-        info.open(map,marker);
+      google.maps.event.addListener(marker, 'click', function() {
+        info.open(map, marker);
       });
+
+      // วนแสดง marker หลายตัว
+      // var marker, i, info;
+      // for (i = 0; i < locations.length; i++) {
+      //   // สร้าง marker
+      //   marker = new google.maps.Marker({
+      //     position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      //     map: maps,
+      //     // title: locations[i][0]
+      //   });
+
+      //   // สร้าง popup
+      //   info = new google.maps.InfoWindow();
+      //   google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      //       return function() {
+      //         info.setContent(locations[i][0]);
+      //         info.open(maps, marker);
+      //       }
+      //     })
+      //     (marker, i)
+      //   );
+      // }
     }
   </script>
 
