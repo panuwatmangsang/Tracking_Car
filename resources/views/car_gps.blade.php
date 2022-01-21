1aa1dbd4c3f3ae9a48c1331f6055d6373dd3a6f6
panuwatmangsang <48234038+panuwatmangsang@users.noreply.github.com>
2022-01-21 17:08:52 +0700

    get data from db error

--- resources/views/car_gps.blade.php
+++ resources/views/car_gps.blade.php
@@ -6,47 +6,52 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tracking Car</title>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
 <body>
-  <div id="googleMap" style="width:100%;height:400px;"></div>
+  <div id="googleMap" style="width:100%;height:600px;"></div>
 
   <script>
     function myMap() {
       var position = {
-        lat: 18.9306783,
-        lng: 99.1841769
+        lat: 18.8945854,
+        lng: 99.0098195
       }
       var maps;
 
+      url = `car_gps_data`;
+
       var locations = [
         ['Toyota', 18.8945854, 99.0098195],
-        ['Isuzu', 18.8935056, 98.9871569],
-        ['Honda', 18.8902647, 98.9852353]
+        ['Isuzu', 18.8842692, 99.0047842],
+        ['Honda', 18.8937735, 99.0034448],
+        ['Nissan', 18.8987441, 99.0133599]
       ];
 
+      // var jsonObj = [
+      //   {"location":"Toyota", "lat":"18.8945854", 'lng':"99.0098195"},
+      //   {"location":"Isuzu", "lat":"18.8842692", 'lng':"99.0047842"},
+      //   {"location":"Honda", "lat":"18.8937735", 'lng':"99.0034448"},
+      //   {"location":"Nissan", "lat":"18.8987441", 'lng':"99.0133599"},
+      // ]
+
       var mapProp = {
-        center: new google.maps.LatLng(18.9306783, 99.1841769),
-        // center: position,
-        zoom: 10,
+        // center: new google.maps.LatLng(18.9306783, 99.1841769),
+        center: position,
+        zoom: 5,
       };
 
       // แสดงแผนที่
       var maps = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
       // แสดง/สรา้ง marker
-      var marker = new google.maps.Marker({
-        position: position,
-        map: maps
-      });
-
-      // แสดง info
-      var info = new google.maps.InfoWindow({
-        content: '<div style="font-size:15px;">Toyota</div>'
-      });
-      google.maps.event.addListener(marker, 'click', function() {
-        info.open(map, marker);
-      });
+      // var marker = new google.maps.Marker({
+      //   icon: "images/car-icon.png",
+      //   // position: position,
+      //   map: maps
+      // });
 
       // วนแสดง marker หลายตัว
       // var marker, i, info;
@@ -55,6 +60,7 @@
       //   marker = new google.maps.Marker({
       //     position: new google.maps.LatLng(locations[i][1], locations[i][2]),
       //     map: maps,
+      //     icon: "images/car-icon.png"
       //     // title: locations[i][0]
       //   });
 
@@ -69,6 +75,30 @@
       //     (marker, i)
       //   );
       // }
+
+      // ดึงข้อมูลจาก Database
+      var markers, info;
+      $.getJSON(url, function(jsonObj) {
+        console.log(url);
+        for (let i = 0; i < markers.length; i++) {
+
+          const marker_position = new google.maps.LatLng(markers[i][5], markers[i][6])
+          // const checkDistance = google.maps.geometry.spherical.computeDistanceBetween(marker_position, ceter_position)
+
+
+
+          let marker = new google.maps.Marker({
+            position: new google.maps.LatLng(markers[i][5], markers[i][6]),
+            // icon: (markers[i][6] == 'fulltime') ? '../image/bluepoint03.png' : '../image/greenpoint02.png',
+            map: map,
+            optimized: false,
+            title: markers[i][1],
+          });
+          marker._infowindow = new google.maps.InfoWindow({
+            content: markers[i][1]
+          });
+        }
+      });
     }
   </script>
 
